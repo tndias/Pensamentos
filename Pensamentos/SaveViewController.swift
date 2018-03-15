aa4fe2b4136168cc1d30993e4a0de10a2b28fd35
Usuário Convidado <Guest@pa06micro26.local>
2018-03-14 23:03:11 -0300

    Aplicação razoavelmente funcionando

--- Pensamentos/SaveViewController.swift
+++ Pensamentos/SaveViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SaveViewController.swift
 //  Pensamentos
 //
 //  Created by Usuário Convidado on 14/03/18.
@@ -8,14 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SaveViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Teste de alteracao")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,4 +22,3 @@ class ViewController: UIViewController {
 
 
 }
-
